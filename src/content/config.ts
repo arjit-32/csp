22a7e7b05fcbb9394d6c4f9f9977a357a4c37b44
Arjit Sharma <arjit_sharma@canalys.com>
2024-03-14 01:52:09 +0530

    this turing site works for now

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define a `type` and `schema` for each collection
-const categoryCollection = defineCollection({
+const webdev = defineCollection({
   type: 'content', 
   schema: z.object({
     title: z.string(),
@@ -12,9 +12,18 @@ const categoryCollection = defineCollection({
   }),
 });
 
-
+const systemdesign = defineCollection({
+  type: 'content', 
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    course: z.string(),
+    publishDate: z.date().optional()
+  }),
+});
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-   'webdev': categoryCollection
+   'webdev': webdev,
+    'systemdesign': systemdesign
 };
--- src/content/webdev/upandrunning/lec1.md
+++ src/content/webdev/upandrunning/lec1.md
@@ -5,3 +5,31 @@ course: 'upandrunning'
 ---
 
 ## Lecture 1: Introduction to Web Development
+
+```java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
+```
+
+This is testing of markdown file
+
+### Subtitle
+
+#### Sub-subtitle
+
+- List item 1
+- List item 2
+- List item 3
+
+
+1. List item 1
+2. List item 2
+3. List item 3
+
+[Link to Google](https://www.google.com)
+
+![Image](https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png)
+```
